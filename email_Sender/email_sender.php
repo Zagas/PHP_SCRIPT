ec6e436789f2c10df2b20da1ec126038f700183f
Giuseppe Z <giuseppezagaria88@gmail.com>
2018-08-09 12:35:55 +0200

    Perfezionamento Script

--- email_Sender/email_sender.php
+++ email_Sender/email_sender.php
@@ -1,11 +1,11 @@
 <?php
 
-//Esempio con scandeza di polizze assicurative
-function send_email($nome_cognome,$phone,$email,$fax,$nome_polizza,$data_scadenza,$id_polizza,$data_sotto,$recipients){
+//Esempio con scandeza di progetti
+function send_email($nome_cognome,$phone,$email,$fax,$nome_progetto,$data_scadenza,$id_progetto,$data_sotto,$recipients){
     $to = implode(",",$recipients); //array with all email
     //$to      = 'rubino@sharks.net,mrhide@hideme.com';
-    $subject = 'Scadenza '.$nome_polizza.' di '.$nome_cognome.' il '.$data_scadenza;
-    $message = 'La polizza'. $id_polizza.'-'.$nome_polizza.'  di '.$nome_cognome.'  sta scandendo, conttatalo al più presto!
+    $subject = 'Scadenza '.$nome_progetto.' di '.$nome_cognome.' il '.$data_scadenza;
+    $message = 'La polizza'. $id_progetto.'-'.$nome_polizza.'  di '.$nome_cognome.'  sta scandendo, conttatalo al più presto!
     Sottoscritta '.$data_sotto.' e scadrà '.$data_scadenza.'
     
     Email: '.$email.'
